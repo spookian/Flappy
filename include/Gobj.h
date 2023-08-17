91aabc7f1335e4f8c3957b14f0da405d1f6ac229
spookian <ianbow111@gmail.com>
2023-08-16 22:30:44 -0400

    a tiny bit of restructuring

---
+++ include/Gobj.h
@@ -0,0 +1,14 @@
+#ifndef FLAP_GOBJ_H
+#define FLAP_GOBJ_H
+
+#include <Hel/Vector3.h> // doesn't exist yet
+
+namespace gobj
+{
+	class Move
+	{
+		Vector3 velocity();
+	} // placing class definition here
+}
+
+#endif
